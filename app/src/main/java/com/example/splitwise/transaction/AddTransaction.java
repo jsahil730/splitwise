d58747ff557073a7bf71feb4200b55e276bd9ac1
Sahil Jain <49490887+jsahil730@users.noreply.github.com>
2019-11-23 19:41:22 +0530

    Transac frontend

--- app/src/main/java/com/example/splitwise/transaction/AddTransaction.java
+++ app/src/main/java/com/example/splitwise/transaction/AddTransaction.java
@@ -1,5 +1,4 @@
 package com.example.splitwise.transaction;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
