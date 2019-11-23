dd28d09194b10673918334958071f33682ec1086
Sahil Jain <49490887+jsahil730@users.noreply.github.com>
2019-11-23 19:41:22 +0530

    Transac frontend

---
+++ app/src/main/java/com/example/splitwise/transaction/AddTransaction.java
@@ -0,0 +1,16 @@
+package com.example.splitwise.transaction;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.splitwise.R;
+
+public class AddTransaction extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_transaction);
+    }
+}
